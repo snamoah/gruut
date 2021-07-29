e78776cdde55f3b8519a2bc56c006df9ac3144a1
Samuel Amoah <sa.am@programmer.net>
2021-07-30 01:45:16 +0200

    feat(): import font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
-import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>Groot</title>
-        <meta name="description" content="Download Videos and Images from Instagram" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 export default App

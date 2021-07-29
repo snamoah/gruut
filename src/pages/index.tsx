a3da106042917f37839919fb041867bf4ffb99fb
Samuel Amoah <sa.am@programmer.net>
2021-07-30 01:46:44 +0200

    feat(): create ui for above the fold

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
+import Logo from '../components/Logo'
+import styles from '../styles/index.module.css'
+import WomanWithPhone from '../components/illustrations/WomanWithMobile'
+
 const Home = () => {
   return (
-    <>
-      <h1>Groot</h1>
-    </>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Logo />
+      </header>
+      <main>
+        <section className={styles.aboveTheFold}>
+          <aside>
+            <h1 className={styles.title}>
+              Download <br /> Instagram Content
+            </h1>
+            <p className={styles.subtitle}>
+              Easy access to Posts, Reels and IGTV contents
+            </p>
+          </aside>
+          <figure>
+            <WomanWithPhone />
+          </figure>
+        </section>
+        <section>{/* input search bar */}</section>
+        <section>{/* benefits section */}</section>
+        <section>{/* how it works */}</section>
+        <footer></footer>
+      </main>
+    </div>
   )
 }
 

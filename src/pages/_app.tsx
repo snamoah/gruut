c8ebea0b0eaf9f3ea8ea9bd50f0bf0f927674b51
Samuel Amoah <sa.am@programmer.net>
2021-08-05 02:57:44 +0200

    feat(): configure default seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
 import { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="Gruut - Download Instagram Content"
+        description="Easily download Reels, IGTV Videos, Images and Posts from Instagram"
+        openGraph={{
+          type: 'website',
+          url: 'https://gruut.xyz',
+          title: 'Gruut - Download Instagram Content',
+          description:
+            'Easily download Reels, IGTV Videos, Images and Posts from Instagram',
+          images: [
+            {
+              url: '/og-image.png',
+              width: 1200,
+              height: 630,
+              alt: 'Gruut OG Image',
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
 
         <body>

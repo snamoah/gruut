9896ed398e3b719eff2e1d20e0a68dee4d68a0ce
Samuel Amoah <sa.am@programmer.net>
2021-08-06 12:57:05 +0200

    refactor(): index page to use Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Logo from '../components/Logo'
+import Layout from '../components/Layout'
 import InputBox from '../components/InputBox'
 import styles from '../styles/index.module.css'
 import Eye from '../components/illustrations/Eye'
@@ -43,100 +43,73 @@ const Home = () => {
   }
 
   return (
-    <>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Logo />
+    <Layout>
+      <section className={styles.aboveTheFold}>
+        <aside>
+          <h1 className={styles.title}>
+            Download <br /> Instagram Content
+          </h1>
+          <p className={styles.subtitle}>Easy access to Posts, Reels and IGTV contents</p>
+        </aside>
+        <figure>
+          <WomanWithPhone />
+        </figure>
+      </section>
+      <section className={styles.inputBox}>
+        <InputBox loading={submitting} onSubmit={onSubmit} />
+      </section>
+      <section className={styles.benefits}>
+        <header>
+          <h2>Benefits</h2>
+          <p>We offer these and more!</p>
         </header>
-        <main className={styles.main}>
-          <section className={styles.aboveTheFold}>
-            <aside>
-              <h1 className={styles.title}>
-                Download <br /> Instagram Content
-              </h1>
-              <p className={styles.subtitle}>
-                Easy access to Posts, Reels and IGTV contents
-              </p>
-            </aside>
+
+        <div>
+          <div>
             <figure>
-              <WomanWithPhone />
+              <GiftBox />
             </figure>
-          </section>
-          <section className={styles.inputBox}>
-            <InputBox loading={submitting} onSubmit={onSubmit} />
-          </section>
-          <section className={styles.benefits}>
-            <header>
-              <h2>Benefits</h2>
-              <p>We offer these and more!</p>
-            </header>
+            <h3>Free</h3>
+            <p>Downloads all your contents free of charge</p>
+          </div>
+          <div>
+            <figure>
+              <Eye />
+            </figure>
+            <h3>All content</h3>
+            <p>Download images, videos, Reels and IGTV videos</p>
+          </div>
+          <div>
+            <figure>
+              <Hand />
+            </figure>
+            <h3>Easy</h3>
+            <p>Dowloading content is as easy as copy and paste</p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.howItWorks}>
+        <div>
+          <div>
+            <h2>How it works</h2>
 
-            <div>
-              <div>
-                <figure>
-                  <GiftBox />
-                </figure>
-                <h3>Free</h3>
-                <p>Downloads all your contents free of charge</p>
-              </div>
-              <div>
-                <figure>
-                  <Eye />
-                </figure>
-                <h3>All content</h3>
-                <p>Download images, videos, Reels and IGTV videos</p>
-              </div>
-              <div>
-                <figure>
-                  <Hand />
-                </figure>
-                <h3>Easy</h3>
-                <p>Dowloading content is as easy as copy and paste</p>
-              </div>
-            </div>
-          </section>
-          <section className={styles.howItWorks}>
-            <div>
-              <div>
-                <h2>How it works</h2>
-
-                <ol>
-                  <li>
-                    <span>
-                      Copy the link of any post, video, reel, IGTV video from instagram
-                    </span>
-                  </li>
-                  <li>
-                    <span>Paste link in the input field above and press enter</span>
-                  </li>
-                  <li>
-                    <span>Preview video or image and then download</span>
-                  </li>
-                </ol>
-              </div>
-              <figure></figure>
-            </div>
-          </section>
-        </main>
-      </div>
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          Built with{' '}
-          <span role="img" aria-label="Red Heart">
-            ❤️
-          </span>{' '}
-          and{' '}
-          <span role="img" aria-label="Coffee">
-            ☕
-          </span>{' '}
-          by{' '}
-          <b>
-            <a href="https://snamoah.dev" rel="noreferrer" target="_blank">
-              @snamoah
-            </a>
-          </b>
-        </p>
-      </footer>
+            <ol>
+              <li>
+                <span>
+                  Copy the link of any post, video, reel, IGTV video from instagram
+                </span>
+              </li>
+              <li>
+                <span>Paste link in the input field above and press enter</span>
+              </li>
+              <li>
+                <span>Preview video or image and then download</span>
+              </li>
+            </ol>
+          </div>
+          <figure></figure>
+        </div>
+      </section>
       {data.posts && (
         <DownloadModal
           open={downloadModalOpen}
@@ -152,7 +125,7 @@ const Home = () => {
         username={data.username}
         profileUrl={data.profile_pic_url}
       />
-    </>
+    </Layout>
   )
 }
 

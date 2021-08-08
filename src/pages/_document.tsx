90b93bca5b4096e97910465e437827577f9612e7
Samuel Amoah <sa.am@programmer.net>
2021-08-08 15:51:04 +0200

    feat(): add ga script to app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,23 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="manifest" href="/manifest.json" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_CODE}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+    
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_CODE}', {
+                  page_path: window.location.pathname
+                });
+              `,
+            }}
+          ></script>
         </Head>
 
         <body>
